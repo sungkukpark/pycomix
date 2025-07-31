0e3864aafabdbeea375e51a6967117b366d9af53
Sungkuk Park <sungkukshawnpark@gmail.com>
2025-07-31 11:42:14 +0200

    rename author

--- pycomix/__init__.py
+++ pycomix/__init__.py
@@ -5,7 +5,7 @@ A PyQt6-based comic reader application for reading digital comics.
 """
 
 __version__ = "0.1.0"
-__author__ = "PyComix Team"
+__author__ = "Sungkuk Park"
 __description__ = "A modern comic reader application built with PyQt6"
 
 from pycomix.app import PyComixApp
